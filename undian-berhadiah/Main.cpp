b4e8475d5ab0819a6bb05f8bcd815aeadf0ff5fa
Ali Zulfikar <alizulfikar032@gmail.com>
2022-09-08 13:56:32 +0700

    Do: Undian-berhadiah

---
+++ undian-berhadiah/Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int N, X;
+
+    cin >> N >> X;
+    int *arr = new int[N];
+    int arrLow[N];
+    int counter = 0;
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    int lowest = abs(arr[0] - X);
+
+    for (int i = 0; i < N; i++) {
+        int diff = abs(X - arr[i]);
+        if (diff <= lowest) {
+            lowest = diff;
+            arrLow[counter++] = arr[i];
+        }
+    }
+
+    for (int i = 0; i < counter; i++) {
+        for (int j = 0; j < counter; j++) {
+            if (arrLow[i] < arrLow[j]) {
+                int temp = arrLow[i];
+                arrLow[i] = arrLow[j];
+                arrLow[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < counter; i++) {
+        cout << arrLow[i] << endl;
+    }
+    
+}
